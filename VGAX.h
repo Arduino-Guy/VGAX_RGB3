ccf3a64315936580582d539ff3d6714a722d36ce
Arduino-Guy <hebbar.prashasth@gmail.com>
2025-06-01 19:17:39 -0400

    Update VGAX.h

--- VGAX.h
+++ VGAX.h
@@ -1,66 +1,47 @@
-/*
-VGAX_RGB3 Library - Modified from original VGAX by Sandro Maffiodo
-Adds support for 3-bit RGB (8 color) video output on Arduino UNO.
+#ifndef __VGAX_RGB3_LIBRARY__
+#define __VGAX_RGB3_LIBRARY__
 
-Original VGAX source: https://github.com/smaffer/vgax
-Modified by: [Your Name]
-
-Supports 120x60 pixels, 1 byte per pixel (only lower 3 bits used for RGB color).
-*/
-
-#ifndef VGAX_RGB3_H
-#define VGAX_RGB3_H
-
-#define VGAX_VERSION "RGB3-1.0.0"
-
-#ifdef ARDUINO
 #include <Arduino.h>
-#endif
 
 #define VGAX_WIDTH 120
 #define VGAX_HEIGHT 60
-#define VGAX_SIZE (VGAX_WIDTH * VGAX_HEIGHT)  // 7200 bytes
 
-// Framebuffer: 1 pixel per byte (lower 3 bits used for color)
-extern byte vgaxfb[VGAX_SIZE];
+// 3 bits per pixel => 120 pixels * 3 bits = 360 bits per line => 45 bytes per line
+#define VGAX_BWIDTH 45
+#define VGAX_BSIZE (VGAX_BWIDTH * VGAX_HEIGHT)
 
-// A virtual timer that increments with VSYNC
-extern unsigned long vtimer;
+extern volatile uint8_t vgaxfb[VGAX_BSIZE]; // framebuffer
 
-class VGAX {
+// Pins for RGB output (Arduino UNO PORTD)
+#define PIN_BLUE  5 // PD5
+#define PIN_RED   6 // PD6
+#define PIN_GREEN 7 // PD7
+
+#define COLOR_BLACK   0b000
+#define COLOR_BLUE    0b001
+#define COLOR_RED     0b010
+#define COLOR_MAGENTA 0b011
+#define COLOR_GREEN   0b100
+#define COLOR_CYAN    0b101
+#define COLOR_YELLOW  0b110
+#define COLOR_WHITE   0b111
+
+class VGAX_RGB3 {
 public:
   static void begin(bool enableTone = true);
   static void end();
 
-  // Set a pixel at (x, y) to a 3-bit color (0-7)
-  static inline void putpixel(byte x, byte y, byte color) {
-    if (x >= VGAX_WIDTH || y >= VGAX_HEIGHT) return;
-    color &= 0b00000111;
-    vgaxfb[y * VGAX_WIDTH + x] = color;
-  }
+  static void clear(uint8_t color = COLOR_BLACK);
 
-  // Get the 3-bit color of the pixel at (x, y)
-  static inline byte getpixel(byte x, byte y) {
-    if (x >= VGAX_WIDTH || y >= VGAX_HEIGHT) return 0;
-    return vgaxfb[y * VGAX_WIDTH + x] & 0b00000111;
-  }
+  static void putpixel(uint8_t x, uint8_t y, uint8_t color);
+  static uint8_t getpixel(uint8_t x, uint8_t y);
 
-  // Fill entire screen with a single color
-  static void clear(byte color);
-
-  // Approximate millisecond counter (based on 60Hz vsync)
-  static inline unsigned long millis() {
-    return vtimer * 16;
-  }
-
-  // Approximate microsecond counter
-  static inline unsigned long micros() {
-    return vtimer * 16000;
-  }
-
-  // Sound functions (optional)
   static void tone(unsigned int frequency);
   static void noTone();
+
+  static void delay(int msec);
+
+  static unsigned long millis();
 };
 
 #endif
